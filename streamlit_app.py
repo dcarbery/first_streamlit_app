ddd9b756f4d83c369d59b20b4195f1c6b191b3e2
dcarbery <115799608+dcarbery@users.noreply.github.com>
2022-10-14 16:38:25 +0100

    Update streamlit_app.py
    add multiselect user tool

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
+# import streamlit and pandas libraries
 import streamlit
 import pandas
 
+# build page of title, headers and text
 streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
@@ -11,5 +13,11 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# bring in my fruit list from S3 bucket and display on app
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
