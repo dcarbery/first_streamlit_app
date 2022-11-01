d96f43ed1dc0a72abbf0cbb6cc3029392d6c3fd4
dcarbery <115799608+dcarbery@users.noreply.github.com>
2022-11-01 12:26:35 +0000

    Update streamlit_app.py
    added requests import and webpage to access

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 # import streamlit and pandas libraries
 import streamlit
 import pandas
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 # build page of title, headers and text
 streamlit.title("My Mom's New Healthy Diner")
