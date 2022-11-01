6e8e60590e1cf525dc3cee68864e5044161eec44
dcarbery <115799608+dcarbery@users.noreply.github.com>
2022-11-01 12:30:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,6 @@
 # import streamlit and pandas libraries
 import streamlit
 import pandas
-import requests
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 # build page of title, headers and text
 streamlit.title("My Mom's New Healthy Diner")
@@ -26,3 +22,8 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
