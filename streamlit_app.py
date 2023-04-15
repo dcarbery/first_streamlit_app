04feaadb52d69ac74fb77009ef33546dc75b9431
dcarbery <115799608+dcarbery@users.noreply.github.com>
2023-04-15 18:35:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 # import streamlit and pandas libraries
-import streamlit
-import pandas
+import streamlit as st
+import pandas as pd
 
 sheet_url = "https://docs.google.com/spreadsheets/d/1tADOzIdSGwTCUXE0Uz3uNIRVrMqFk47nhwJLNGVr4Pg/edit?usp=sharing"
 df = pd.DataFrame(sheet_url)
 # build page of title, headers and text
-streamlit.title("FOF Summer Tournament")
+st.title("FOF Summer Tournament")
 
 
 # write your own comment - what does this do?
-streamlit.dataframe(df)
+st.dataframe(df)
