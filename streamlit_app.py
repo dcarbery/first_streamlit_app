9deda81ce94acdae968ecb857392d94bcc0a9955
dcarbery <115799608+dcarbery@users.noreply.github.com>
2023-04-15 19:54:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 # Connect to the Google Sheet
 sheet_id = "1tADOzIdSGwTCUXE0Uz3uNIRVrMqFk47nhwJLNGVr4Pg"
-#sheet_name = "results"
-url = r"https://docs.google.com/spreadsheets/d/" + sheet_id + "edit#gid=0"
+sheet_name = "results"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+#df = pd.read_csv(url, dtype=str)
 df = pd.read_csv(url)
 
 # build page of title, headers and text
