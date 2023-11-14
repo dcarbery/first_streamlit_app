8ff83bd7a2076d5253ad258de8343ce2a4e65fa1
dcarbery <115799608+dcarbery@users.noreply.github.com>
2023-11-14 11:32:30 +0000

    Update streamlit_app.py
    return script to be relevant to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,7 @@
 import streamlit as st
 import pandas as pd
 
-
-# Connect to the Google Sheet
-sheet_id = "1tADOzIdSGwTCUXE0Uz3uNIRVrMqFk47nhwJLNGVr4Pg"
-sheet_name = "results"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-#df = pd.read_csv(url, dtype=str)
-df = pd.read_csv(url)
-
-# build page of title, headers and text
-st.title("FOF Summer Tournament")
-
-for age in df.Age.unique():
-  for group in df.Group.unique():
-    df2=df.loc[ (df['Age'] == age) & (df['Group'] == group),    ]
-    df2.index=['Match ' + str(x) for x in range(1, 11)]
-    
-    st.subheader(age + ' Group ' + group + ' (Pitch ' + df2.iloc[0, 2].astype(str) + ')')
-    # write your own comment - what does this do?
-    st.dataframe(df2.loc[:, 'Time':])
-    
-    st.divider()
-
+st.title('My Parents Healthy New Diner')
     
 
     
